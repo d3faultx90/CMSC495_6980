941104dceebc40ac51ca2880d092b7ca2e18ef12
d3faultx90 <86384421+d3faultx90@users.noreply.github.com>
2021-07-05 17:32:22 -0400

    Update MySQL_sampe_data.sql
    Modified sample data after testing new properties for table. Tested constraints on specific fields for Status and Roles.

--- AWS/MySQL_sampe_data.sql
+++ AWS/MySQL_sampe_data.sql
@@ -1,7 +1,8 @@
 /*
 Created by: Zachary Young
+Updated by: Zachary Young
 Created on: 07/01/2021
-Last edited: 07/03/2021
+Last edited: 07/05/2021
 Created for: CMSC495
 
 MySQL
@@ -14,7 +15,6 @@ Objectives
 
 */
 
-
 /* populate basic data */
 -- users
 INSERT INTO users(Username, Fname, Lname, Role) VALUES ("zyoung", "Zachary", "Young", 0);
@@ -26,14 +26,14 @@ INSERT INTO users(Username, Fname, Lname, Role) VALUES ("mbalth", "Marsha", "Bal
 -- populate passwords... admins only have access to this table
 
 -- inventory
-INSERT INTO inventory(Name, Description, Price, Quantity) VALUES ("Green Apples", "Savory, stem and all.", 1.50, 125);
-INSERT INTO inventory(Name, Description, Price, Quantity) VALUES ("Idaho Potato", "Mash it, bake it, fry it, grill it.", 2.99, 1214);
-INSERT INTO inventory(Name, Description, Price, Quantity) VALUES ("Bell Pepper", "Freshest bell pepper this side of the Atlantic.", 1.99, 5);
-INSERT INTO inventory(Name, Description, Price, Quantity) VALUES ("Dozen Toilet Paper", "Softest brand around, double ply.", 18.99, 35);
+INSERT INTO inventory(Name, Description, Category, WholeSalePrice, RetailPrice, Quantity) VALUES ("Green Apples", "Savory, stem and all.", "Fruit", 0.25, 1.50, 125);
+INSERT INTO inventory(Name, Description, Category, WholeSalePrice, RetailPrice, Quantity) VALUES ("Idaho Potato", "Mash it, bake it, fry it, grill it.", "Vegitable", 0.30, 2.99, 1214);
+INSERT INTO inventory(Name, Description, Category, WholeSalePrice, RetailPrice, Quantity) VALUES ("Bell Pepper", "Freshest bell pepper this side of the Atlantic.", "Vegitable", 0.15, 1.99, 5);
+INSERT INTO inventory(Name, Description, Category, WholeSalePrice, RetailPrice, Quantity) VALUES ("Dozen Toilet Paper", "Softest brand around, double ply.", "Other", 5.15, 18.99, 35);
 
 -- purchase history
-INSERT INTO orders(EmployeeID, ItemID, Description, UnitPrice, TotalPrice, Quantity, OrderDate, Status) VALUES (2, 1, "Need more Apples. Flying off the shelves.", 1.50, 150, 100, '2020-06-19 19:34:02', 1);
-INSERT INTO orders(EmployeeID, ItemID, Description, UnitPrice, TotalPrice, Quantity, OrderDate, Status) VALUES (3, 1, "Need more Apples. Flying off the shelves.", 1.50, 300, 200, '2021-06-19 17:34:02', 1);
-INSERT INTO orders(EmployeeID, ItemID, Description, UnitPrice, TotalPrice, Quantity, OrderDate, Status) VALUES (3, 2, "People love potatos.", 2.99, 2990, 1000, '2021-07-01 21:02:25', 0);
-INSERT INTO orders(EmployeeID, ItemID, Description, UnitPrice, TotalPrice, Quantity, OrderDate, Status) VALUES (2, 4, "We need the paper.", 18.99, 3323.25, 175, '2021-07-02 10:18:35', 0);
+INSERT INTO orders(EmployeeID, ItemID, Description, FoodCategory, WholeSaleUnitPrice, WholeSaleTotalPrice, Quantity, OrderDate, Status) VALUES (2, 1, "Need more Apples. Flying off the shelves.", "Fruit", 0.25, 25, 100, '2020-06-19 19:34:02', 1);
+INSERT INTO orders(EmployeeID, ItemID, Description, FoodCategory, WholeSaleUnitPrice, WholeSaleTotalPrice, Quantity, OrderDate, Status) VALUES (3, 1, "Need more Apples. Flying off the shelves.", "Vegitable", 0.30, 60, 200, '2021-06-19 17:34:02', 1);
+INSERT INTO orders(EmployeeID, ItemID, Description, FoodCategory, WholeSaleUnitPrice, WholeSaleTotalPrice, Quantity, OrderDate, Status) VALUES (3, 2, "People love potatos.", "Vegitable", 0.15, 150, 1000, '2021-07-01 21:02:25', 0);
+INSERT INTO orders(EmployeeID, ItemID, Description, FoodCategory, WholeSaleUnitPrice, WholeSaleTotalPrice, Quantity, OrderDate, Status) VALUES (2, 4, "We need the paper.", "Other", 5.15, 180.15, 175, '2021-07-02 10:18:35', 0);
 
