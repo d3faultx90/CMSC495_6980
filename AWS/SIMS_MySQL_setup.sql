380e97a2b8535df579b8346269ca6f26bbcb4204
d3faultx90 <86384421+d3faultx90@users.noreply.github.com>
2021-07-12 22:13:10 -0400

    SIMS_MySQL_setup.sql supports more ...
    Added limiting user functions to only the SIMS_app_data and created new table.

--- AWS/SIMS_MySQL_setup.sql
+++ AWS/SIMS_MySQL_setup.sql
@@ -2,33 +2,30 @@
 Created by: Zachary Young
 Updated by: Zachary Young
 Created on: 07/01/2021
-Last edited: 07/05/2021
+Last edited: 07/12/2021
 Created for: CMSC495
 
 Tested platform(s): 
   mysql  Ver 15.1 Distrib 10.3.28-MariaDB, for Linux (x86_64) using readline 5.1
 
 Ensure the following are applied:
-- AWS account differs from MySQL account and from Application account
-Objectives
-  - Secure MySQL instance, Databases and Tables
-  - Create new users that can query applicaiton database
+  - Create new users that can query application database
   - Lock down new user permissions
-  - Create new applicaiton database
-  - Create 4 tables: Users, Inventory, Order History, and App Passwords
 
 HELPFUL COMMANDS:
-  drop app tables: "
-        DROP TABLE IF EXISTS inventory_app_data.orders;
-        DROP TABLE IF EXISTS inventory_app_data.users;
-        DROP TABLE IF EXISTS inventory_app_data.inventory;
-        "
-  drop app mysql.user: "
-        DROP USER IF EXISTS 'inventory_admin'@'localhost';
-        DROP USER IF EXISTS 'inventory_user'@'localhost';
+  drop app tables:
+DROP TABLE IF EXISTS SIMS_app_data.orders;
+DROP TABLE IF EXISTS SIMS_app_data.users;
+DROP TABLE IF EXISTS SIMS_app_data.inventory;
+DROP TABLE IF EXISTS SIMS_app_data.sales;
+  
+  drop app mysql.user:
+DROP USER IF EXISTS 'SIMS_admin'@'localhost';
+DROP USER IF EXISTS 'SIMS_admin_bkup'@'localhost';
+DROP USER IF EXISTS 'zyoung5';EXISTS 'SIMS_admin_bkup'@'localhost';
         "
   drop app database: "
-        USE mysql; DROP DATABASE IF EXISTS inventory_app_data;
+        USE mysql; DROP DATABASE IF EXISTS SIMS_app_data;
         "
         
 EXAMPLE USAGE:
@@ -37,49 +34,59 @@ mysql -u root -p < ./SIMS_MySQL_setup.sql
 */
 
 
-/* create MySQL users for DataBase connection */
-SELECT ' [+] Configuring users ...' as '';
+/* create original application admin for DataBase connection */
+SELECT ' [+] Creating users ...' as '';
 USE mysql;
-CREATE USER IF NOT EXISTS 'inventory_admin'@'localhost' IDENTIFIED BY 'password';
-CREATE USER IF NOT EXISTS 'inventory_user'@'localhost' IDENTIFIED BY 'password';
-SELECT ' [+] Users configured:' as '';
-SELECT user FROM mysql.user WHERE user LIKE "inventory%";
+CREATE USER IF NOT EXISTS 'SIMS_admin' IDENTIFIED BY 'SIMS_Sup3r_C0mplex!';
+CREATE USER IF NOT EXISTS 'SIMS_admin_bkup' IDENTIFIED BY 'SIMS_Sup3r_B&kup!';
+CREATE USER IF NOT EXISTS 'zyoung5' IDENTIFIED BY 'P@ssw0rd';
+SELECT ' [+] Users created:' as '';
+SELECT user FROM mysql.user WHERE user LIKE "SIMS%";
+SELECT user FROM mysql.user;
+
 
 /* create database if it does not exist */
 SELECT ' [+] Configuring database ...' as '';
-CREATE DATABASE IF NOT EXISTS inventory_app_data;
+CREATE DATABASE IF NOT EXISTS SIMS_app_data;
 SELECT ' [+] database built:' as '';
 SHOW databases;
 
-USE inventory_app_data;
+/* harden default SIMS admin account */
+SELECT ' [+] Updating user permissions ...' as '';
+SHOW GRANTS FOR 'SIMS_admin';
+SHOW GRANTS FOR 'SIMS_admin_bkup';
+REVOKE ALL PRIVILEGES, GRANT OPTION FROM 'SIMS_admin';
+REVOKE ALL PRIVILEGES, GRANT OPTION FROM 'SIMS_admin_bkup';
+GRANT ALL PRIVILEGES ON SIMS_app_data.* TO 'SIMS_admin';
+GRANT ALL PRIVILEGES ON SIMS_app_data.* TO 'SIMS_admin_bkup';
+SHOW GRANTS FOR 'SIMS_admin';
+SHOW GRANTS FOR 'SIMS_admin_bkup';
+SELECT ' [+] Users permissions updated.' as '';
 
-/* limit user to only application data/users */
-/*
--- remove user ability to query any database
-SHOW GRANTS FOR 'inventory_admin'@'localhost'; 
-SHOW GRANTS FOR 'inventory_user'@'localhost'; 
-REVOKE ALL PRIVILEGES ON %database_name% FROM 'inventory_admin'@'localhost'; 
-REVOKE ALL PRIVILEGES ON %database_name% FROM 'inventory_user'@'localhost'; 
-SHOW GRANTS FOR 'inventory_admin'@'localhost'; 
-SHOW GRANTS FOR 'inventory_user'@'localhost'; 
-
--- GRANT inventory_admin for the inventory_app_data database
-GRANT ALL PRIVILEGES ON %database_name% TO 'inventory_admin'@'localhost';
-
--- GRANT inventory_user for inventory_app_data database
-GRANT ALL PRIVILEGES ON %database_name% TO 'inventory_user'@'localhost';
-
--- check permission of users
-SHOW GRANTS FOR 'inventory_admin'@'localhost';
-SHOW GRANTS FOR 'inventory_user'@'localhost';
-
+/* All users created will have a SQL account and be \
+    granted limited permissions. 
+    
+    Administrator = 0
+    Supervisor    = 1
+    User          = 2
+    
+    examples:
+CREATE USER IF NOT EXISTS 'zyoung5' IDENTIFIED BY 'P@ssw0rd';
+SHOW GRANTS FOR 'zyoung5';
+REVOKE ALL PRIVILEGES, GRANT OPTION FROM 'zyoung5';
+GRANT SELECT, INSERT ON SIMS_app_data.* TO 'zyoung5';
+SHOW GRANTS FOR 'zyoung5';
+    
 */
+
+USE SIMS_app_data;
 
 /* create tables if they do not exists */
 SELECT ' [+] Configuring tables ...' as '';
 
 -- User application table
 -- Role: 0 = admin, 1 = approver, 2 = employee
+-- username is their sql name
 CREATE TABLE IF NOT EXISTS users(
     UserID INT NOT NULL PRIMARY KEY AUTO_INCREMENT, 
     Username VARCHAR(60) UNIQUE NOT NULL, 
@@ -109,7 +116,7 @@ COMMENT="The inventory table is used for managing inventory items."
 -- OrderID Random GUID assigned by Java Function .... 
 CREATE TABLE IF NOT EXISTS orders(
     OrderID INT NOT NULL PRIMARY KEY AUTO_INCREMENT, 
-    EventID INT NOT NULL, 
+    OrderEventID INT NOT NULL, 
     EmployeeID INT NOT NULL,
     ItemID INT NOT NULL,
     Description VARCHAR(120) NOT NULL,
@@ -125,10 +132,27 @@ CREATE TABLE IF NOT EXISTS orders(
 COMMENT="The orders table is used for managing whole sale orders placed."
 ;
 
+--
+CREATE TABLE IF NOT EXISTS sales(
+    SalesID INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    SalesEventID INT NOT NULL,
+    EmployeeID INT NOT NULL,
+    ItemID INT NOT NULL,
+    Description VARCHAR(120) NOT NULL,
+    FoodCategory VARCHAR(60) NOT NULL, 
+    SalePrice DECIMAL(8,2) NOT NULL,
+    Quantity INT NOT NULL, 
+    OrderDate DATETIME NOT NULL,
+    FOREIGN KEY (EmployeeID) REFERENCES users(UserID),
+    FOREIGN KEY (ItemID) REFERENCES inventory(InventoryID)
+)
+COMMENT="The sales table is used for tracking customer sales."
+;
+
 SELECT ' [+] tables built:' as '';
 SHOW tables;
 
 -- check those tables
 SELECT ' [+] Final report:' as '';
-SELECT TABLE_COMMENT,CREATE_TIME,UPDATE_TIME FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_SCHEMA='inventory_app_data';
+SELECT TABLE_COMMENT,CREATE_TIME,UPDATE_TIME FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_SCHEMA='SIMS_app_data';
 SELECT ' [!] COMPELETE.' as '';
