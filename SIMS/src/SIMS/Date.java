41323102c76cb901a7245570a2f3427e726f4085
d3faultx90 <86384421+d3faultx90@users.noreply.github.com>
2021-07-30 19:33:34 -0400

    Update Date.java

--- SIMS/src/SIMS/Date.java
+++ SIMS/src/SIMS/Date.java
@@ -5,44 +5,60 @@ import java.time.format.DateTimeFormatter;
 
 public class Date {
 
-	static LocalDateTime getDateTime() {
-		/**
-		 * Method to retrieve local system date and time
-		 * 
-		 * @return date as LocalDateTime object
-		 */
+    private static LocalDateTime getDateTime() {
+        /**
+         * Method to retrieve local system date and time
+         * 
+         * @return date  as LocalDateTime object
+         */
+    	
+         LocalDateTime date = LocalDateTime.now();
+         return  date;
+         
+    } // end of getDate()
+    
+    protected String formatDate() {
+        /**
+         * Method to format local system date
+         * 
+         * @return dateToString as String object
+         */
 
-		LocalDateTime date = LocalDateTime.now();
-		return date;
+    	LocalDateTime df = getDateTime();
 
-	} // end of getDate()
+        // convert 'df' variable to String and format like MM-dd-yyyy
+        String dateToString = df.format(DateTimeFormatter.ofPattern("MM-dd-YYYY"));
+        return dateToString;
+    } // end of formatDate()
+    
+    protected String formatDateTime() {
+        /**
+         * Method to format local system date and time
+         * 
+         * @return dateTimeToString as String object
+         */
+    	
+    	LocalDateTime df = getDateTime();
 
-	protected static String formatDate() {
-		/**
-		 * Method to format local system date
-		 * 
-		 * @return dateToString as String object
-		 */
+        // convert 'df' variable to String and format like MM-dd-yyyy HH:mm:ss
+        String dateTimeToString = df.format(DateTimeFormatter.ofPattern("MM-dd-YYYY HH:mm:ss"));
+        return dateTimeToString;
+    } // end of formatDateTime()
 
-		LocalDateTime df = getDateTime();
+    protected static String sqlDateTime() {
+        /**
+         * Method to format local system date and time
+         * 
+         * @return dateTimeToString as String object
+         */
+    	
+    	LocalDateTime df = getDateTime();
 
-		// convert 'date' variable to String and format like MM-dd-yyyy
-		String dateToString = df.format(DateTimeFormatter.ofPattern("MM-dd-YYYY"));
-		return dateToString;
-	} // end of formatDate()
-
-	protected String formatDateTime() {
-		/**
-		 * Method to format local system date and time
-		 * 
-		 * @return dateTimeToString as String object
-		 */
-
-		LocalDateTime df = getDateTime();
-
-		// convert 'date' variable to String and format like MM-dd-yyyy
-		String dateTimeToString = df.format(DateTimeFormatter.ofPattern("MM-dd-YYYY HH:mm:ss"));
-		return dateTimeToString;
-	} // end of formatDateTime()
-
+        // convert 'df' variable to String and format like '2011-12-03+01:00'
+        String dateTimeToString = df.format(DateTimeFormatter.ISO_OFFSET_DATE);
+        return dateTimeToString;
+        
+    } // end of formatDateTime()
+    
+        
 }
